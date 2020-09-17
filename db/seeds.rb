1e8a26e1f98368195bdd82f8a125a1615176c902
Marcel Schmidt <schmidt@analyse-konzepte.de>
2020-09-17 08:19:20 +0200

    Integrted technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@
   )
 end
 
-p "3 Topics created"
+puts "3 Topics created"
 
 10.times do |i|
   Blog.create!(
@@ -22,7 +22,7 @@ p "3 Topics created"
   )
 end
 
-p "10 blog posts created"
+puts "10 blog posts created"
 
 5.times do |i|
   Skill.create!(
@@ -31,13 +31,13 @@ p "10 blog posts created"
   )
 end
 
-p "5 skills created"
+puts "5 skills created"
 
 8.times do |i|
   Portfolio.create!(
     title: "Portfolio title: #{i}",
     subtitle: "Ruby on Rails",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquiputs ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image:"https://via.placeholder.com/350x200"
   )
@@ -47,10 +47,18 @@ end
   Portfolio.create!(
     title: "Portfolio title: #{i}",
     subtitle: "Angular",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquiputs ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image:"https://via.placeholder.com/350x200"
   )
 end
 
-p "9 portfolio items created"
+puts "9 portfolio items created"
+
+3.times do |i|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{i}"
+  )
+end
+
+puts "3 technologies created"
